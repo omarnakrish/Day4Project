6e65d53256ece05a572fa21c2b339abd0afdde7b
omarnakrish <82983234+omarnakrish@users.noreply.github.com>
2021-05-25 19:25:38 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,6 +2,12 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
+        // created anew project
+        // enabled version control
+        // added .gitignore with  .idea  , target fplder and .iml file
+        // made initial commt by keeping track of all files
+        // shared project on github
+
     }
 
 }
